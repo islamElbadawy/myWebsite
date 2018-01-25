a1a6d1aad88e03e4942e79b1ab9bf1dedf309105
islamElbadawy <islam.201211@gmail.com>
2018-01-26 00:51:37 +0200

    myWebsite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule
