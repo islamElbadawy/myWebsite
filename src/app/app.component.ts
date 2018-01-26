7f4062884cddb84261451bf7908967e583284114
islamElbadawy <islam.201211@gmail.com>
2018-01-26 02:54:54 +0200

    Tow way data binding & HTML Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {FirstComponent} from './first/first.component';
 import { LoginService } from './Services/login.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,10 +10,25 @@ import { LoginService } from './Services/login.service';
 })
 export class AppComponent {
   title = 'islam M. Elbadawy';
+  name:string="islamElbadawy";
   constructor(private mylogin:LoginService){
-    var first = this.mylogin.login("admin" , "admin2");
-    var seconde = this.mylogin.login("admin" , "admin");
+    //var first = this.mylogin.login("admin" , "admin2");
+    //var seconde = this.mylogin.login("admin" , "admin");
 
-    console.log(first,seconde);
+    //console.log(first,seconde);
+    console.log(this.name);
+    
+    }
+    show(){
+      console.log(this.name);
+  }
+  select(){
+    console.log("You have selected an element");
+  }
+  blur(){
+    console.log("You have lost focus")
+  }
+  focus(){
+    console.log("you have focus");
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
@@ -13,7 +13,7 @@ import { LoginService } from './Services/login.service';
     FirstComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [LoginService],
   bootstrap: [AppComponent]
